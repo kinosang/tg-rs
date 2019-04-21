d56cb1767f03c8b94e12c26de101e274ce4b7fa4
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-04-21 20:02:43 +0000

    Deploy tg-rs/tg-rs to github.com/tg-rs/tg-rs.git:gh-pages

--- implementors/alloc/string/trait.ToString.js
+++ implementors/alloc/string/trait.ToString.js
@@ -1,5 +1,4 @@
 (function() {var implementors = {};
-implementors["carapax_session"] = [{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/alloc/string/trait.ToString.html\" title=\"trait alloc::string::ToString\">ToString</a> for <a class=\"struct\" href=\"carapax_session/struct.SessionKey.html\" title=\"struct carapax_session::SessionKey\">SessionKey</a>",synthetic:false,types:["carapax_session::session::SessionKey"]},];
 implementors["tgbot"] = [{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/alloc/string/trait.ToString.html\" title=\"trait alloc::string::ToString\">ToString</a> for <a class=\"enum\" href=\"tgbot/types/enum.ChatId.html\" title=\"enum tgbot::types::ChatId\">ChatId</a>",synthetic:false,types:["tgbot::types::chat::ChatId"]},{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/alloc/string/trait.ToString.html\" title=\"trait alloc::string::ToString\">ToString</a> for <a class=\"enum\" href=\"tgbot/types/enum.ParseMode.html\" title=\"enum tgbot::types::ParseMode\">ParseMode</a>",synthetic:false,types:["tgbot::types::primitive::ParseMode"]},];
 
             if (window.register_implementors) {
