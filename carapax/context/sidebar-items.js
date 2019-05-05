64b1605b27e0b766ab29c88f9d7417c8f6a626fe
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-05-05 11:38:23 +0000

    Deploy tg-rs/tg-rs to github.com/tg-rs/tg-rs.git:gh-pages

---
+++ carapax/context/sidebar-items.js
@@ -0,0 +1,1 @@
+initSidebarItems({"enum":[["Entry","A view into a single location in an `Map`, which may be vacant or occupied."]],"struct":[["Context","Context for handlers"],["OccupiedEntry","A view into a single occupied location in an `Map`."],["VacantEntry","A view into a single empty location in an `Map`."]]});
